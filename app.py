50c3cdcdace18877dbc5a7cf234483f4c972e802
NeoM404 <mthi45martin@gmail.com>
2024-07-28 00:12:34 +0200

    Implemented functionality for saving and retrieving items from PostgreSQL database, exposed via an endpoint

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import os
 from flask import Flask, request
 from psycopg_pool import ConnectionPool
 
-app = Flask(__name__)
-
 def dbConnect():
     """Create a connection pool to connect to Postgres"""
     db_host = os.getenv('DB_HOST')
@@ -25,6 +23,7 @@ def dbConnect():
 # Create a connection pool to Postgres
 pool = dbConnect()
 
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
@@ -73,3 +72,65 @@ def volume():
             volume = 'No data'
 
         return volume, 200
+    
+def save_item(priority, task, table, pool):
+    """Save an item/task to the database"""
+
+    # Connect to an existing Database
+    with pool.connection() as conn:
+
+        # Open a cursor to operate database connectiom
+        with conn.cursor() as cur:
+
+            # prepare the database query
+            query = f"INSERT INTO {table} (priority, task) VALUES (%s, %s)"
+
+            # send the query to PostgreSQL with the actual values 
+            cur.execute(query, (priority, task))
+
+            # Make the changes to the database persistent
+            conn.commit()
+
+def get_items(table, pool):
+    """Get all items/tasks from the database"""
+
+    # Connect to an existing Database
+    with pool.connection() as conn:
+
+        # Open a cursor to operate database connectiom
+        with conn.cursor() as cur:
+
+            # prepare the database query
+            query = f"SELECT item_id, priority, task  FROM {table}"
+
+            # send the query to Postgres
+            cur.execute(query)
+
+            items = []
+
+            for rec in cur:
+                item = {
+                    'item_id': rec[0],
+                    'priority': rec[1],
+                    'task': rec[2]
+                }
+                items.append(item)
+
+            # return a list of items
+            # results = cur.fetchall()
+            return items
+        
+@app.route('/items', methods=['GET', 'POST'])
+def items():
+    match request.method:
+        case 'GET':
+            items = get_items('item', pool)
+
+            return items, 200
+        case 'POST':
+            req = request.get_json()
+            save_item(req['priority'], req['task'], 'item', pool)
+
+            return {'Message': 'Item Saved!'}, 201
+        case _:
+            return {'Message': 'Invalid request'}, 405
